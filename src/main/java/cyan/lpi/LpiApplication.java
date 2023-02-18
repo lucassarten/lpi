2bf8d346d94df1caab67f761527321a289b9b176
Lucas Sarten <sartenluca@ecs.vuw.ac.nz>
2023-02-19 00:24:21 +1300

    Create Maven project

---
+++ src/main/java/cyan/lpi/LpiApplication.java
@@ -0,0 +1,13 @@
+package cyan.lpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cyan/lpi/LpiApplicationTests.java
@@ -0,0 +1,13 @@
+package cyan.lpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
